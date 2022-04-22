adf83e9db135e6b002515184719d0ebe55e2ca14
szegediviktor <szeg.viktor@gmail.com>
2022-04-22 16:47:41 +0200

    :tada:

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>WeEat</h1>
+            <h5>Share your plate, share your emotions</h5>
+        </div>
+    );
+}
+
+export default App;
