37575421ef8f6c176d11044f1a214419397fde16
szegediviktor <szeg.viktor@gmail.com>
2022-04-26 20:45:03 +0200

    change the placeholder

--- frontend/src/plates/pages/AddNewPlate.jsx
+++ frontend/src/plates/pages/AddNewPlate.jsx
@@ -1,7 +1,8 @@
+import Card from "../../shared/components/UI-elements/Card";
 import "./addNewPlate.css";
 
 const AddNewPlate = () => {
-    return <h1>AddNewPlate comp</h1>;
+    return <h1>Coming soon</h1>;
 };
 
 export default AddNewPlate;
