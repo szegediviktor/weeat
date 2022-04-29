fd80ad4c30a8c7f900fc491197cae5d4bcd4ce0f
szegediviktor <szeg.viktor@gmail.com>
2022-04-29 16:33:26 +0200

    add authentication context (usecontext)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import UserPlates from "./plates/pages/UserPlates";
@@ -8,25 +8,53 @@ import MainNav from "./shared/components/navigation/MainNav";
 import UpdatePlate from "./plates/pages/UpdatePlate";
 import Auth from "./user/pages/Auth";
 
+import { AuthenticationContext } from "./shared/context/auth-usecontext";
+import { useCallback, useState } from "react";
+
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const login = useCallback(() => {
+        setIsLoggedIn(true);
+    }, []);
+    const logout = useCallback(() => {
+        setIsLoggedIn(false);
+    }, []);
+
+    let routes;
+
+    if (isLoggedIn) {
+        routes = (
+            <>
+                <Route path="/" element={<Users />} />
+                <Route path="/:userId/plates" element={<UserPlates />} exact />
+                <Route path="/plates/add" element={<AddNewPlate />} />
+                <Route path="/plates/:plateId" element={<UpdatePlate />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </>
+        );
+    } else {
+        routes = (
+            <>
+                <Route path="/" element={<Users />} />
+                <Route path="/:userId/plates" element={<UserPlates />} exact />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="*" element={<Navigate to="/auth" />} />
+            </>
+        );
+    }
+
     return (
-        <div className="App">
-            <BrowserRouter>
-                <MainNav />
-                <Routes>
-                    <Route path="/" element={<Users />} />
-                    <Route
-                        path="/:userId/plates"
-                        element={<UserPlates />}
-                        exact
-                    />
-                    <Route path="/plates/add" element={<AddNewPlate />} />
-                    <Route path="/plates/:plateId" element={<UpdatePlate />} />
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="*" element={<Users />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <AuthenticationContext.Provider
+            value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+        >
+            <div className="App">
+                <BrowserRouter>
+                    <MainNav />
+                    <Routes>{routes}</Routes>
+                </BrowserRouter>
+            </div>
+        </AuthenticationContext.Provider>
     );
 }
 
