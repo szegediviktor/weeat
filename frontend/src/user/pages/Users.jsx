8f489f29fa05c248fb80a46e0b4b79f985b5b81b
szegediviktor <szeg.viktor@gmail.com>
2022-05-02 10:32:10 +0200

    change to random dummy-datas

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -2,24 +2,12 @@ import UserList from "../components/UserList";
 
 const Users = () => {
     const DUMMY_USERS = [
-        // {
-        //     id: "user1",
-        //     name: "Rasmus Kofoed",
-        //     img: "https://www.ateriet.com/wp-content/uploads/2016/04/Geranium-69-Rasmus-Kofoed-Head-Chef-Co-owner.jpg-1-2.jpg",
-        //     sharedPlates: 5,
-        // },
         {
             id: "user1",
-            name: "Széll Tamás",
-            img: "https://image-api.nosalty.hu/nosalty/images/articles/7f/cs/Yc3l1ZE5jmujpz6FdXiDFNzI8EMfkbmiqdeARMvj.jpeg?w=640&h=640&fit=crop&s=ebd438bff0eb5b4c463ae94e2eec23ad",
+            name: "Viktor Szegedi",
+            img: "https://media-exp1.licdn.com/dms/image/C4E03AQGLVtriKkw4TA/profile-displayphoto-shrink_200_200/0/1648285172635?e=1654128000&v=beta&t=jhk_g_oMJ-NDByDl3Vur29QcXfuZLo-Hp7e-F4yei2o",
             sharedPlates: 2,
         },
-        // {
-        //     id: "user3",
-        //     name: "Dalnoki Bence",
-        //     img: "https://vince.hu/media-library/less-than-p-greater-than-dalnoki-bence-kepviseli-a-magyar-szineket-less-than-p-greater-than.jpg?id=29558094",
-        //     sharedPlates: 1,
-        // },
     ];
 
     return <UserList userData={DUMMY_USERS} />;
