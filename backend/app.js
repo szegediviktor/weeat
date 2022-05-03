004e27cc572110527f526f119dcc5170f0299a00
szegediviktor <szeg.viktor@gmail.com>
2022-05-03 17:11:01 +0200

    add error handling to unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const platesRoutes = require("./routes/plates-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/plates", platesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError("Could not find this URL", 404);
+    throw error;
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
