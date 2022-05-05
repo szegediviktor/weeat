93d4112698a69e1ab9f4de110e51efc2d8edeb3d
szegediviktor <szeg.viktor@gmail.com>
2022-05-05 16:13:06 +0200

    change the missing img

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -5,7 +5,7 @@ const Users = () => {
         {
             id: "user1",
             name: "Viktor Szegedi",
-            img: "https://media-exp1.licdn.com/dms/image/C4E03AQGLVtriKkw4TA/profile-displayphoto-shrink_200_200/0/1648285172635?e=1654128000&v=beta&t=jhk_g_oMJ-NDByDl3Vur29QcXfuZLo-Hp7e-F4yei2o",
+            img: "https://images.unsplash.com/photo-1579935110464-fcd041be62d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHlvZGF8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
             sharedPlates: 2,
         },
     ];
